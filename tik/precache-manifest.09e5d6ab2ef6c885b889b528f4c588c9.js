b70ac9f156ce19400004b14a0a892afb59340003
awebird <awebird@163.com>
2020-05-26 16:55:04 +0100

    tik-static-path-fix

---
+++ tik/precache-manifest.09e5d6ab2ef6c885b889b528f4c588c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41c9bc8d20640ff0e44444434e2dd8e6",
+    "url": "/tik/index.html"
+  },
+  {
+    "revision": "ed2ab8cc15264cc2066d",
+    "url": "/tik/static/css/main.3936a687.chunk.css"
+  },
+  {
+    "revision": "1a21e92e24477c464780",
+    "url": "/tik/static/js/2.eb54353c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tik/static/js/2.eb54353c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed2ab8cc15264cc2066d",
+    "url": "/tik/static/js/main.6b11adcb.chunk.js"
+  },
+  {
+    "revision": "e0dcb47b8869625078ba",
+    "url": "/tik/static/js/runtime-main.9a63e4aa.js"
+  }
+]);
--- tik/service-worker.js
+++ tik/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f50fbcbf74d98f2724590eab3e99a18e.js"
+  "/tik/precache-manifest.09e5d6ab2ef6c885b889b528f4c588c9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tik/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
